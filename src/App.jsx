e93e1a9c1a1e91294c01e3f3eb22e38eb0496c8c
sponkurtus2 <sponkurtus3@gmail.com>
2023-09-02 18:01:15 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useRef } from "react";
+import song from "./song.mp3";
+import catMewing from "./mewing.png";
+
+function App() {
+  const audio = new Audio(song);
+  audio.loop = true;
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [showText, setShowText] = useState(true); // Agregamos un estado para controlar la visibilidad del texto
+
+  const toggleAudio = () => {
+    audio.loop = true;
+    audio.play();
+    setIsPlaying(true);
+    setShowText(false); // Ocultamos el texto cuando se inicia la reproducción
+  };
+
+  return (
+    <div className="App">
+      {/* Usamos una expresión condicional para mostrar el texto o la imagen */}
+      {showText ? (
+        <h1>Empezo para ti?</h1>
+      ) : (
+        <img src={catMewing} alt="Cat Mewing" />
+      )}
+
+      {/* Usamos una expresión condicional para mostrar el botón solo si no se está reproduciendo */}
+      {!isPlaying && <button onClick={toggleAudio}>Ya empezo para ti?</button>}
+    </div>
+  );
+}
+
+export default App;
